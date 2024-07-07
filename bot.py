5feb822164e3acc1e6ba5c317e2d15e1e14563fa
TGKoin <vitya6942@gmail.com>
2024-07-08 00:21:50 +0300

    Add files via upload
    START BOT

---
+++ bot.py
@@ -0,0 +1,19 @@
+import telebot
+
+# Ваш токен бота
+TOKEN = '7042638042:AAEUuqWYxiVJ8M5OThIZd3hPQ0g_CuGF3Yk'
+
+# URL на вашу веб-страницу
+WEBPAGE_URL = 'https://yourdomain.com'
+
+# Создание экземпляра бота
+bot = telebot.TeleBot(TOKEN)
+
+# Обработчик команды /start
+@bot.message_handler(commands=['start'])
+def send_welcome(message):
+    bot.reply_to(message, f'Привет! Переходите по ссылке для получения информации: {WEBPAGE_URL}')
+
+# Запуск бота
+if __name__ == '__main__':
+    bot.polling()
